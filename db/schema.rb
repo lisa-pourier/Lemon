c5f251c976bf32ed9911fa5d78af4459f0692b0f
lisa pourier <lisapourier@gmail.com>
2024-02-27 15:06:45 +0100

    offerseeds created

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2024_02_27_132316) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
