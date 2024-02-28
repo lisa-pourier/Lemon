60f4fe37b8d721ad9468bb046f12ecceed09c1be
lisa pourier <lisapourier@gmail.com>
2024-02-28 14:41:04 +0100

    made improvements to CreateOfferButton

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,5 +2,5 @@ class Offer < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
 
-  validates :company, :description, :price, :location, presence: true
+  validates :company, :description, :price, :location, :photo, presence: true
 end
