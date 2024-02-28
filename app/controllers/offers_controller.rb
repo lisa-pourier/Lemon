d56ff186e167b0e299a626422dcd17b25a325af2
lisa pourier <lisapourier@gmail.com>
2024-02-28 13:00:19 +0100

    create offer button on navigation

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,11 +1,32 @@
 class OffersController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
+  def index
+    @offers = Offer.all
+  end
+
   def show
     @offer = Offer.find(params[:id])
   end
 
-  def index
-    @offers = Offer.all
+  def new
+    @offer = Offer.new
   end
+
+  def create
+    @offer = Offer.new(offer_params)
+    @offer.user = current_user
+    if @offer.save
+      raise
+      redirect_to offer_path(@offer)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+private
+def offer_params
+  params.require(:offer).permit(:company, :description, :price, :location)
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'offers/index'
-  get 'offers/show'
-  get 'offers/index'
-  get 'home/index'
+  # get 'offers/index'
+  # get 'offers/show'
+  # get 'offers/index'
+  # get 'home/index'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -10,8 +10,8 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  get '/offers', to: 'offers#index', as: 'offers'
+  # get '/offers', to: 'offers#index', as: 'offers'
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :offers, only: [:index, :show]
+  resources :offers, only: [:index, :show, :new, :create]
 end
